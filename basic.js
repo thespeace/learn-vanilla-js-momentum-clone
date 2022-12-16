1f7aefc4f82c58914ee712212972dc138a29c185
thespeace <thespeace1@gmail.com>
2022-12-16 18:39:04 +0900

    221216 18:38 basic remind

--- basic.js
+++ basic.js
@@ -19,7 +19,7 @@ arrayOrList.push("addItem")
 const variablesTypeRule = 'Always "const", sometimes "let", "var" never!';
 
 const Object = {
-  property : "item1",
+  property1 : "item1",
   function : function(){
 
   }
@@ -30,5 +30,35 @@ Object.newProperty = "addNewItem";
 function function1(argument1, argument2) {
     console.log(argument1 + argument2); /*function's body*/
 }
-
 function1(5, 10);
+
+const calculator = {
+    plus   : (a,b)=>a+b,
+    minus  : (a,b)=>a-b,
+    times  : (a,b)=>a*b,
+    divide : (a,b)=>a/b,
+    power  : (a,b)=>a**b
+}
+
+const plusResult = calculator.plus(1,2);
+alert(plusResult);
+/*const age = parseInt(prompt("How old are you?"));
+
+if(isNaN(age) || age < 0){
+    alert("Please write a real positive number..");
+}else if(age === 8){
+    alert("Go to Elementary School!");
+}else if(age < 18){
+    alert("You are too young..");
+}else if(age >= 18 && age <= 50){
+    alert("You can drink!");
+}else if(age > 50 && age <= 80){
+    alert("You should exercise..");
+}else if(age > 80){
+    alert("You can do whatever you want.");
+}*/
+
+
+function clickAndShow(id, show) {
+    document.getElementById(id).innerText += " "+show;
+}
