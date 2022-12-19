f2dd7d1fe1d53d80aa14adf57f69aed689f2ace1
thespeace <thespeace1@gmail.com>
2022-12-19 17:37:04 +0900

    221219 17:36 vanilla-js basic finish

--- table.js
+++ table.js
@@ -1,22 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     alert("Welcome, Vanilla Js World!");
 })
-/*
-$(function(){
-    $(".container .view").on("click", function(){
-        alert("1");
-    });
-});
-const calculator = {
-    plus   : (a,b)=>a+b,
-    minus  : (a,b)=>a-b,
-    times  : (a,b)=>a*b,
-    divide : (a,b)=>a/b,
-    power  : (a,b)=>a**b
-}*/
-let rows = document.getElementsByTagName('td');
-function moveDown(el){
-    if(el.getElementsByTagName('td:first-child') && el.parentNode.nextElementSibling !== null){
+
+function toggle(el){
+    if(el.parentNode.nextElementSibling !== null){
         if(el.parentNode.nextElementSibling.style.display === 'none'){
             el.parentNode.nextElementSibling.style.display = '';
         }else{
@@ -26,3 +13,20 @@ function moveDown(el){
         alert("준비된 코드가 없습니다.");
     }
 }
+
+/*// 6
+const sixEl = document.getElementById('sixEl');
+const sixEvent = () => { sixEl.innerText = "Got your ID! (특정 ID.element 읽고, 변경)"; };
+
+// 7
+const sevenEl = document.getElementsByClassName("sevenClass");
+console.log(sevenEl)
+const sevenEvent = () => {
+    for (let i = 0; i < sevenEl.length; i++) {
+    sevenEl[i].innerText = "Got your className["+i+"]!";
+}};*/
+
+
+const clickSevenEl = () => { alert(document.querySelector("#sevenEl").innerHTML); }
+document.querySelector("#sevenClick").addEventListener("click", clickSevenEl);
+
